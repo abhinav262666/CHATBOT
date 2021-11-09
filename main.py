a1c1625c71519669087527f0901328eff0787175
Rahul-Chugh-1999 <rahul.chugh.2018@gmail.com>
2021-11-09 20:51:08 +0530

    Minor changes

--- main.py
+++ main.py
@@ -4,16 +4,28 @@ original_data_file = "Data/data1.csv"
 search_space = 1000
 no_of_products = 3
 retention  = 0.8
+exit_keywords = ["no", "exit", "finish", "done", "nothing", "no thanks"]
+start_sentences = [
+    "Which product are you interested in?", 
+    "Hi, let me help you find the product you are interested in:",
+    ]
+details_sentences = [
+    "Noted. What else?",
+    "Go on:",
+    "OK! continue:"
+]
 
 from numpy.core.records import record
 from src.model import Retrieval_Model
 import pandas as pd
+import random
 
 model = Retrieval_Model()
 pdf = pd.read_csv(processed_data_file)
 odf = pd.read_csv(original_data_file)
 
-request  = input("Which product are you interested in? ")
+start_sentence = random.choice(start_sentences)
+request  = input(start_sentence+" ")
 id_score = model.get_similar_items(request, search_space)
 ids = id_score.index.to_list()
 cur_score = id_score['ensemble_similarity']
@@ -29,8 +41,9 @@ def print_product(product_id : str) -> None:
     print("Product Price", list(product['price'])[0])
 
 while True:
-    details = input("Noted. What else? ")
-    if details == "nothing":
+    details_sentence = random.choice(details_sentences)
+    details = input(details_sentence+" ")
+    if details in exit_keywords:
         suggested_ids = sorted(avg_score, key=avg_score.get, reverse=True)[:no_of_products]
         for i in range(no_of_products):
             print("Product no ", i+1,":")
